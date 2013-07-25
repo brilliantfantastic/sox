771ca244e51a0e6c0fd6361933ba8f2900a60306
Jamie Wright <jamie@brilliantfantastic.com>
2013-07-25 17:16:45 -0400

    Use an NSXMLDocument to generate the xml

--- lib/sox/request_options.rb
+++ lib/sox/request_options.rb
@@ -1,12 +1,22 @@
 module Sox
   class RequestOptions
     def request
-      "<request method=\"#{@method}\"></request>"
+      generate_xml
     end
 
     def initialize(method, options={})
       @method = method
       @options = options
+    end
+
+    private
+
+    def generate_xml
+      root = NSXMLElement.alloc.initWithName 'request'
+      root.addAttribute NSXMLNode.attributeWithName('method', stringValue: @method)
+      document = NSXMLDocument.alloc.initWithRootElement root
+      data = document.XMLDataWithOptions NSXMLDocumentTidyXML
+      NSString.alloc.initWithData(data, encoding: NSUTF8StringEncoding)
     end
   end
 end
